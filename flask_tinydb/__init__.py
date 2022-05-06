2b889cd7ce7c20d5ac3d4758c7ec6ea70063de32
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-07 00:32:27 +0430

    fix setup files

--- flask_tinydb/__init__.py
+++ flask_tinydb/__init__.py
@@ -2,4 +2,4 @@ from flask_tinydb.storage import YAMLStorage
 from flask_tinydb.tinydb import TinyDB  # type: ignore
 
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-from flask_tinydb import __version__
-
 
 def readme():
     with open("README.rst", encoding="utf8") as f:
@@ -8,29 +6,5 @@ def readme():
 
 
 setup(
-    name="Flask-tinydb",
-    version=__version__,
-    url="http://github.com/mmdbalkhi/flask-tinydb",
-    license="BSD",
-    author="Komeil Parseh",
-    author_email="ahmdparsh129@gmail.com",
-    description="flask-tinydb is a Flask extension that provides a TinyDB database.",
-    long_description=readme(),
-    long_description_content_type="text/x-rst",
-    packages=["flask_tinydb"],
-    py_modules=["flask_tinydb"],
-    zip_safe=False,
-    include_package_data=True,
-    platforms="any",
-    python_requires=">=3.7",
-    install_requires=["Flask", "tinydb", "pyyaml"],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
+    install_requires=["Flask >= 2.1.0", "tinydb >= 4.0.0", "pyyaml >= 6.0"],
 )
