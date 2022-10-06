da6e438618ee7b74feebb2a9651be1fd57e5d276
komeil Parseh <ahmdparsh129@gmail.com>
2022-10-06 21:20:43 +0330

    update setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup
 
-
-def readme():
-    with open("README.rst", encoding="utf8") as f:
-        return f.read()
-
-
+# Dependencies are in setup.py for GitHub's dependency graph.
 setup(
-    install_requires=["Flask >= 2.1.0", "tinydb >= 4.0.0", "pyyaml >= 6.0"],
+    install_requires=["Flask >= 2.1.0", "tinydb >= 4.0.0"],
+    extras_require={"yaml": ["pyyaml >= 6.0"]},
 )
